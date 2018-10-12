5ee8fcaf7735043e247ae3c88aad88b04aec7390
shimi003 <shimi003@localhost.com>
2018-10-12 18:34:50 +0900

    templateTest

--- myportal/sdss/urls.py
+++ myportal/sdss/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
+    path(r'', views.index, name='index'),
+    path(r'template/', views.templateTest, name='templateTest'),
 ]
-
--- myportal/sdss/views.py
+++ myportal/sdss/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Hello, world, It's django!")
+
+def templateTest(request):
+    dic = {'name': 'shimi003', 'test': 'tekitoudata'}
+    return render(request, 'sdss.html', dic)
