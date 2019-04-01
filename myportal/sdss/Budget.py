87f8e18b41e632c86bee90cc471691ad22912062
shimi003 <shimi003@localhost.com>
2019-04-01 17:21:04 +0900

    予算一覧の取得にバグがあったので修正

--- myportal/sdss/Budget.py
+++ myportal/sdss/Budget.py
@@ -4,18 +4,20 @@ import sdss.models as db
 from django.db.models import Sum
 import sdss.Utility as u
 
-def getBudget(acc_bot_uid):
+def getBudget(i_acc_bot_uid):
 
-    if u.isIntAndNotZero(acc_bot_uid):
+    if u.isIntAndNotZero(i_acc_bot_uid) == False:
         return 0
     else:
-        qs_acc_list = db.Budget.objects.get(uid=acc_bot_uid)
-        for qs_acc in qs_acc_list:
-            strFrom = qs_acc.from_field
-            strFor = qs_acc.for_field
-            if isTermInNow(strFrom_, strFor):
-                return qs_acc.amount_per_month
+        if db.Budget.objects.filter(acc_bot_uid=i_acc_bot_uid).exists():
+            qs_acc_list = db.Budget.objects.filter(acc_bot_uid=i_acc_bot_uid)
+            for qs_acc in qs_acc_list:
+                strFrom = qs_acc.from_field
+                strFor = qs_acc.for_field
+                if isTermInNow(strFrom, strFor):
+                    return qs_acc.amount_per_month
         return 0
+
 
 def isTermInNow(strFrom, strFor):
     strNow = datetime.now().strftime('%Y%m%d')
