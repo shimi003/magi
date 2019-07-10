7af8d53c11b0cb8f98e59f069db74c961cc8ae9d
shimi003 <shimi003@localhost.com>
2019-07-10 10:17:12 +0900

    bugfix duplicate urls name regist on sdss and temp

--- myportal/temperature/urls.py
+++ myportal/temperature/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path(r'', views.index, name='temp_index'),
-    path(r'regist/', views.regist, name='regist'),
+    path(r'regist/', views.regist, name='geo_regist'),
     path('<path:ymd>/', views.date, name='temp_index'),
 ]
