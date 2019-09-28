3f912028e655613577f58aa10b0034ba8d9285c8
shimi003 <shimi003@localhost.com>
2019-09-29 00:37:41 +0900

    定期支出一括登録機能の実装

--- myportal/sdss/urls.py
+++ myportal/sdss/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path(r'regularly-payment/', views.regularly_view, name='regularly_view'),
     path(r'regularly-payment/add/', views.regularly_add, name='regularly_add'),
     path(r'regularly-payment/regist/', views.regularly_regist, name='regularly_regist'),
+    path(r'reg_regist/', views.regist_regularly_payment, name='regist_regularly_payment'),
     path(r'acc_list/', views.acc_list, name='acc_list'),
     path(r'fixed_asset/', views.fixed_asset, name='fixed_asset'),
     path(r'fixed_asset/addform/', views.fixed_asset_addform, name='fixed_asset_addform'),
